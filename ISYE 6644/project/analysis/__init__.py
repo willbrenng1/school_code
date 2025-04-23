cf4331725996799f807bb270aa33cd89555410be
William Brennan <wbrennan7@gatech.edu>
2025-04-22 22:23:00 -0400

    final project update

---
+++ ISYE 6644/project/analysis/__init__.py
@@ -0,0 +1,8 @@
+from .gol_analysis import (
+    cohens_f,
+    calc_matrix_size_and_initial_percent_alive_group_stats,
+    anova_analysis,
+    required_n_per_group,
+    determine_sample_size,
+    sample_v_final_stat_comparison,
+)
---
+++ ISYE 6644/project/visualization/__init__.py
@@ -0,0 +1,8 @@
+from .gol_visualization import (
+    show_random_sample_runs,
+    show_heat_map,
+    show_end_state_circle_chart,
+    show_initial_percent_alive_ci_avg,
+    show_ci_delta,
+    difference_in_average_ci_percent_alive_groups,
+)
